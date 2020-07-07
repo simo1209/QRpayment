020da6455ffaa1add25cc17f72973e71fd961b16
Simeon Georgiev <simeongeorgiev09@gmail.com>
2020-07-07 15:43:04 +0300

    Added endpoint for generating transactions

--- flaskr/transaction.py
+++ flaskr/transaction.py
@@ -3,29 +3,51 @@ from flask import (
 )
 from flask import jsonify
 from flask import session
+from flask import send_file
 
 from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
 from flaskr.db import DB
 
+import qrcode
+
 bp = Blueprint('transactions', __name__, url_prefix='/transactions')
+
+@bp.route('/create', methods=['POST'])
+@login_required
+def create():
+    transcation_desc = request.json['transaction_desc']
+    amount = float(request.json['amount'])
+    error = None
+
+    if not transcation_desc:
+        error = "Transaction Description must"
+    if not amount or amount <= 0:
+        error = "Amount shouldn't be negative"
+
+    with DB() as db:
+        db.execute("INSERT INTO transactions(seller_id, transaction_desc, amount, status) VALUES (%s, %s, %s, 'Pending') RETURNING id;",
+        (g.account['id'], transcation_desc, amount))
+        id = db.fetchone()['id']
+        img = qrcode.make('QRpayment:{}'.format(id))
+        img.save('qr-codes/{}.png'.format(id))
+        return send_file('../qr-codes/{}.png'.format(id))
+    return error, 400
 
 
 @bp.route('/check', methods=['POST'])
 @login_required
 def check():
     transaction_data = request.json['data'].split(':')
-    print(transaction_data)
     if transaction_data[0] != 'QRpayment':
         print('Unrecognized QR code')
         return 'Unrecognized QR code', 400
     transaction_id = transaction_data[-1]
-    print(transaction_id)
     with DB() as db:
         db.execute(
-            '''SELECT transactions.transaction_desc, 
-                accounts.first_name AS sellers_name FROM transactions 
+            '''SELECT transactions.id, transactions.transaction_desc, transactions.amount,
+                accounts.first_name AS seller_name FROM transactions 
                 LEFT JOIN accounts on transactions.seller_id = accounts.id 
                 WHERE transactions.id = %s''',
             (transaction_id)
@@ -39,7 +61,7 @@ def list():
     with DB() as db:
         db.execute(
             '''SELECT transactions.id, transactions.transaction_desc, transactions.amount, 
-                accounts.first_name AS sellers_name
+                accounts.first_name AS seller_name
                 FROM transactions LEFT JOIN accounts on transactions.seller_id = accounts.id 
                 WHERE buyer_id = %s OR seller_id = %s;''',
             (g.account['id'], g.account['id'])
